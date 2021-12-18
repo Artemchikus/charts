56f60ca735037104ca9c25d1d76adce5bac91177
Artemiks <fil.ar.alex@gmail.com>
2021-12-18 11:26:55 +0300

    добавил поля

--- backend/internal/app/model/stock.go
+++ backend/internal/app/model/stock.go
@@ -4,7 +4,8 @@ type Stock struct {
 	ID            int     `json:"id"`
 	Name          string  `json:"name"`
 	FIGI          string  `json:"figi"`
-	// Amount        int     `json:"amount"`
-	// AnnualRevemue float32 `json:"annual-revenue"`
-	// Capital       string  `json:"capital"`
+	Amount        int     `json:"amount"`
+	AnnualRevemue float32 `json:"annual-revenue"`
+	Capital       string  `json:"capital"`
+	Ticker        string  `json:"ticker"`
 }
